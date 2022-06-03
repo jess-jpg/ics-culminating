15e9c5fb03558dc5a8ccda3248b85615c6a173ca
sophiawang22 <91137351+sophiawang22@users.noreply.github.com>
2022-06-03 10:09:39 -0400

    Add files via upload

---
+++ src/Drink.java
@@ -0,0 +1,24 @@
+/**
+*/
+
+public class Drink extends MenuItem {
+   private String sugarLevel; //none less reg extra
+   private boolean ice; 
+   private String size;
+   
+   public Drink (String name, double price, boolean hot, int quantity, String sugarLevel, boolean ice, String size){
+      super (name, price, hot, quantity);
+      this.sugarLevel = sugarLevel;
+      this.ice = ice;
+      this.size = size;
+   }
+   public String getSugarLevel(){
+      return this.sugarLevel;
+   }
+   public boolean getIce(){
+      return this.ice;
+   }
+   public String getSize(){
+      return this.size;
+   }
+}
