5fbf8fdaaf1a38b0d78189e0a574433649d6a19f
sophiawang22 <91137351+sophiawang22@users.noreply.github.com>
2022-06-03 19:45:58 -0400

    Update Coffee.java

--- src/Coffee.java
+++ src/Coffee.java
@@ -1,4 +1,14 @@
 /**
 */
 public class Coffee extends Drink {
+   private boolean decaf;
+   
+   public Coffee (String name, double price, boolean hot, int quantity, String sugarLevel, boolean ice, char size, boolean decaf){
+      super(name, price, hot, quantity, sugarLevel, ice, size);
+      this.decaf = decaf;
+   }
+   
+   public boolean isDecaf(){
+      return this.decaf;
+   }
 }
