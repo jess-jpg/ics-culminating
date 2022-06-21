aecab49ff1215a0a3e7afb4b2571b9c89ccef485
sophiawang22 <91137351+sophiawang22@users.noreply.github.com>
2022-06-20 21:12:11 -0400

    Update Drink.java

--- src/Drink.java
+++ src/Drink.java
@@ -1,14 +1,39 @@
 /**
+Sophia Wang 
+June 20
+Drink class -- subclass of menuItem and has differernt temperatures, sugar levels, sizes, and ice or not
 */
 
 public class Drink extends MenuItem {
+   /*Attributes*/
+  
+   /**sugar level of drink*/
    private String sugarLevel; //none less regular extra
+   
+   /** ice of drink*/
    private boolean ice; 
+   
+   /** size of drink*/
    private String size;
+   
+   /** temp of drink*/
    private boolean hot;
    
+   
+    /* constructor */
+
+    /**
+     * Name: Drink
+     * Description: Creates a drink
+     * @param name name of drink
+     * @param quantity amount of drink
+     * @param hot temperature of drink
+     * @param sugarLevel sugar level of drink (none, less, regular, extra)
+     * @param ice if drink has ice in it (true/false)
+     * @param size size of drink
+     */
    public Drink (String name, int quantity, boolean hot, String sugarLevel, boolean ice, String size){
-      super (name, quantity);
+      super (name, quantity); // from menuItem
       this.sugarLevel = sugarLevel;
       this.hot = hot;
       if (!this.hot){
@@ -18,12 +43,31 @@ public class Drink extends MenuItem {
       }
       this.size = size;
    }
+   
+   /* accessors */
+
+   /**
+    * Name: getSugarLevel
+    * Description: get drink sugar level
+    * @return sugar level of drink
+   */
    public String getSugarLevel(){
       return this.sugarLevel;
    }
+   /**
+    * Name: getIce
+    * Description: get drink ice
+    * @return if drink order has ice or not
+   */
    public boolean getIce(){
       return this.ice;
    }
+   
+   /**
+    * Name: getSize
+    * Description: get drink size
+    * @return size of drink
+   */
    public String getSize(){
       return this.size;
    }
