1021426e3dced682e9dce9dee4055d3e88fdf6fe
sophiawang22 <91137351+sophiawang22@users.noreply.github.com>
2022-06-21 20:40:10 -0400

    Add files via upload

---
+++ src/DisplayImage.java
@@ -0,0 +1,33 @@
+/**
+Sophia Wang
+june 21
+class to display image -- the menu for our culminating
+*/
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class DisplayImage {
+   public static void main(String[] args) throws IOException {
+   
+      File file = new File("C:\\Users\\sophi\\Downloads\\MealsResized.png");
+      BufferedImage bufferedImage = ImageIO.read(file);
+      
+      ImageIcon imageIcon = new ImageIcon(bufferedImage);
+      JFrame jFrame = new JFrame();
+      
+      jFrame.setLayout(new FlowLayout());
+      
+      jFrame.setSize(100, 100);
+      JLabel jLabel = new JLabel();
+      
+      jLabel.setIcon(imageIcon);
+      jFrame.add(jLabel);
+      jFrame.setVisible(true);
+      
+      jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   }
+}
